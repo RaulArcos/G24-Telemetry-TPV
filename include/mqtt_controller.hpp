2598e72f94c4e27690861b983258db3391de67d6
Raúl Arcos Herrera <1.raularcos@gmail.com>
2024-05-14 20:14:43 +0200

    primer commit archivos comunes

---
+++ include/mqtt_controller.hpp
@@ -0,0 +1,25 @@
+#ifndef MQTTCONTROLLER_HPP
+#define MQTTCONTROLLER_HPP
+
+#include "common/common_libraries.hpp"
+#include "common/aws_certificates.hpp"
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+
+class MQTTController {
+public:
+    MQTTController();
+    void connect();
+    static void callback(char* topic, byte* payload, unsigned int length);
+    void publish_telemetry(const char* topic, const char* message);
+
+private:
+    const char* _mqtt_server = "a3gqsinr6fjc3h-ats.iot.eu-central-1.amazonaws.com";
+    const int _mqtt_port = 8883;
+    WiFiClientSecure _espClient;
+    PubSubClient _client = PubSubClient(_espClient);
+    AWSCertificates _awsCertificates;
+};
+
+
+#endif
